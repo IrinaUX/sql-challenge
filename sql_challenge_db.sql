0c7ca0e1b94dcb38c5f7cdf2eb04a7d2e325ccfd
Irina Kim <irinahpda@gmail.com>
2020-07-05 13:22:39 -0500

    Export pdf and postgresql from quickDBD.

--- sql_challenge_db.sql
+++ sql_challenge_db.sql
@@ -1,56 +1,61 @@
 --Employees table
-DROP TABLE Employees; --Drop the Employees table if needed
+DROP TABLE Employees IF EXISTS; --Drop the Employees table if exists
 CREATE TABLE Employees ( --Create an Employees table
-	emp_no INT PRIMARY KEY,
+	emp_no INT PRIMARY KEY NOT NULL,
 	emp_title_id VARCHAR(10),
 	birth_date DATE,
 	first_name VARCHAR(40),
 	last_name VARCHAR(40),
 	sex VARCHAR(10),
-	hire_date DATE);
+	hire_date DATE
+);
 SELECT * FROM Employees; --Check all columns and rows in Employees table
 SELECT COUNT(*) FROM Employees; --Count all rows in Employees table
 --Note: 300024 employees
 
 --Salaries
-DROP TABLE Salaries; --Drop Salaries table if needed
+DROP TABLE IF EXISTS Salaries; --Drop Salaries table if exists
 CREATE TABLE Salaries ( --Create Salaries table
-	emp_no INTEGER PRIMARY KEY,
-	salary INTEGER);
+	emp_no INTEGER PRIMARY KEY NOT NULL,
+	salary BIGINT
+);
 SELECT * FROM Salaries;
 SELECT COUNT(salaries) FROM Salaries; --300024 employees,
 SELECT salary FROM Salaries --Check salary column
 --GROUP BY salary; --Groups by salary
 
 --Titles
-DROP TABLE Titles; --if needed
+DROP TABLE IF EXISTS Titles; --if exists
 CREATE TABLE Titles ( --Create Titles table
-	title_id VARCHAR(20) PRIMARY KEY,
+	title_id VARCHAR(20) PRIMARY KEY NOT NULL,
 	title VARCHAR(255)
 );
 SELECT * FROM Titles;
 
 --Department manager
-DROP TABLE Dept_manager;
+DROP TABLE IF EXISTS Dept_manager;
 CREATE TABLE Dept_manager (
-	dept_no VARCHAR(20),
-	emp_no INT,
-	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no));
-
+	dept_no TEXT,
+	emp_no BIGINT PRIMARY KEY NOT NULL, --INT is not big enough - error when import csv
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+);
 SELECT * FROM Dept_manager;
 SELECT COUNT(*) FROM Dept_manager; --24 departments
 
---Department_Employees
-DROP TABLE Dept_emp; -- if needed
+--Dept_Emp
+DROP TABLE IF EXISTS Dept_emp; -- if exists
 CREATE TABLE Dept_emp (
-	emp_no INT,
-	dept_no VARCHAR(255),
-	PRIMARY KEY(emp_no, dept_no));
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(255) NOT NULL,
+	PRIMARY KEY(emp_no, dept_no)
+);
 SELECT * FROM Dept_emp;
 
 --Departments
-DROP TABLE Departments; --if needed
+DROP TABLE IF EXISTS Departments; --if needed
 CREATE TABLE Departments (
 	dept_no VARCHAR(255) PRIMARY KEY,
-	dept_name VARCHAR(255));
-
+	dept_name VARCHAR(255),
+	FOREIGN KEY (dept_no) REFERENCES Dept_emp(emp_no, dept_no)
+);
+SELECT * FROM Departments;
