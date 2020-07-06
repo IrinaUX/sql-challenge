685a8cdf975da13c1589fd34379fa723fa64ed5a
Irina Kim <irinahpda@gmail.com>
2020-07-05 20:58:07 -0500

    Complete the queries part.

--- sql_challenge_db.sql
+++ sql_challenge_db.sql
@@ -88,7 +88,49 @@ SELECT     Dept_manager.dept_no, Departments.dept_name, Dept_manager.emp_no, Emp
 FROM       Dept_manager
 LEFT JOIN  Departments
 ON         Dept_manager.dept_no = Departments.dept_no
-LEFT JOIN Employees
+LEFT JOIN  Employees
 ON         Dept_manager.emp_no = Employees.emp_no;
+
+--4. List the department of each employee with the following information: 
+	--a) employee number, last name, first name, and department name
+SELECT     Employees.emp_no, Employees.last_name, Employees.first_name, Dept_emp.dept_no, Departments.dept_name
+FROM       Employees
+JOIN       Dept_emp
+ON         Employees.emp_no = Dept_emp.emp_no
+JOIN       Departments
+ON         Dept_emp.dept_no = Departments.dept_no;
+
+--5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B":
+SELECT     Employees.first_name, Employees.last_name, Employees.sex
+FROM 	   Employees
+WHERE      Employees.first_name = 'Hercules'
+AND        last_name LIKE 'B%';
+	
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name:
+SELECT     Employees.emp_no, Employees.last_name, Employees.first_name, Departments.dept_name --Dept_emp.dept_no 
+FROM       Employees
+LEFT JOIN  Dept_emp
+ON         Employees.emp_no = Dept_emp.emp_no
+LEFT JOIN  Departments
+ON         Dept_emp.dept_no = Departments.dept_no
+WHERE      Departments.dept_name = 'Sales';
+
+--7. List all employees in the Sales and Development departments, 
+	--a) including their employee number, last name, first name, and department name:
+SELECT     Employees.emp_no, Employees.last_name, Employees.first_name, Departments.dept_name --Dept_emp.dept_no 
+FROM       Employees
+LEFT JOIN  Dept_emp
+ON         Employees.emp_no = Dept_emp.emp_no
+LEFT JOIN  Departments
+ON         Dept_emp.dept_no = Departments.dept_no
+WHERE      Departments.dept_name = 'Sales' 
+OR         Departments.dept_name = 'Development';
+
+--8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name:
+SELECT     COUNT (Employees.last_name), Employees.last_name --Employees.last_name
+FROM       Employees
+GROUP BY   Employees.last_name --as grouped_last_names
+ORDER BY   COUNT (Employees.last_name) ASC
+
 
 
